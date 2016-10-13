19a4703f92667900e25052aac1d36f0b354a55e7
Sam Lakerveld <dark@novaember.com>
2016-10-13 21:54:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/novaember/hanzeschedule/HanzeSchedule.kt
@@ -0,0 +1,13 @@
+package com.novaember.hanzeschedule
+
+import android.app.Activity
+import android.os.Bundle
+
+public class HanzeSchedule : Activity() {
+
+    /** Called when the activity is first created. */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
