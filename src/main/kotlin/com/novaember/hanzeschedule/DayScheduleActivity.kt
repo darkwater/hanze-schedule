fddceb8370dd637ca4816307c6d5d78f1ab50bfa
Sam Lakerveld <dark@novaember.com>
2016-11-15 15:14:06 +0100

    Added documentation to DayScheduleActivity

--- src/main/kotlin/com/novaember/hanzeschedule/DayScheduleActivity.kt
+++ src/main/kotlin/com/novaember/hanzeschedule/DayScheduleActivity.kt
@@ -16,22 +16,47 @@ import com.android.volley.Response
 
 import kotlinx.android.synthetic.main.activity_weekschedule.*
 
+/**
+ * Displays a per-day schedule of a given week.
+ *
+ * Activity arguments:
+ *     int weekNumber (required) Week number to show
+ *     int day        (def. 0)   Jump to day index in 0..4
+ *
+ * @see DayScheduleViewPager
+ * @see DayScheduleFragment
+ */
 class DayScheduleActivity : AppCompatActivity() {
+
+    /**
+     * Initializes the activity.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dayschedule)
+
+        // Use the Toolbar's home button as a back button
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
+        // Add an adapter for this week
         val weekNumber = intent.getIntExtra("weekNumber", 0)
-        val day = intent.getIntExtra("day", 0)
         scheduleViewPager.adapter = DaySchedulePagerAdapter(Session.activeSchedule!!.getWeek(weekNumber), this)
+
+        // Jump to a given day
+        val day = intent.getIntExtra("day", 0)
         scheduleViewPager.currentItem = day
 
+        // Set activity title
         setTitle("Week $weekNumber")
     }
 
+    /**
+     * Closes this activity (go back) when pressing the Toolbar's home button.
+     *
+     * @return always true
+     */
     override fun onSupportNavigateUp(): Boolean {
         finish()
 
